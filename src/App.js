db9c22fff35f5ede4fb94bb61e8a3e737cf774da
marinawangmw <marinawangmw@gmail.com>
2020-07-26 21:48:23 -0300

    +Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
+
+  const HatPage = () => (
+  	<div>
+	    <h1> Hat's Page </h1>
+  	</div>
+  )	
+
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
