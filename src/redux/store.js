c43b31a04d0e2120ff26aa66cc98c7b252201957
marinawangmw <marinawangmw@gmail.com>
2020-08-08 12:24:51 -0300

    removing logging from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,13 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+//arranco con array vacío
+const middlewares = [];
+
+// para pushear mis logs si estuviera en ambiente de desarrollo
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
